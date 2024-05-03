b65181a26b734a022f158dfd6871a05ba00a6356
Mykyta Leshchenko <cs.zevd1999@gmail.com>
2024-05-03 11:30:17 +0300

    Getting pizza data from the API

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,34 @@
 import { MenuList } from './MenuList.jsx'
+import { useEffect, useState } from 'react'
 
 export function Menu() {
+	const [menu, setMenu] = useState([])
+
+	useEffect(() => {
+		const getAllPizzas = async () => {
+			try {
+				const res = await fetch(
+					'https://react-fast-pizza-api.onrender.com/api/menu'
+				)
+				const data = await res.json()
+
+				if (!res.ok) {
+					throw new Error('Error fetching pizza data.')
+				}
+
+				setMenu(data.data)
+			} catch (error) {
+				console.error(error.message)
+			}
+		}
+		getAllPizzas()
+	}, [])
+
 	return (
 		<>
-			<MenuList />
+			{menu.map((pizzas, index) => (
+				<MenuList key={index} pizzas={pizzas} />
+			))}
 		</>
 	)
 }
--- src/components/Menu/MenuList.jsx
+++ src/components/Menu/MenuList.jsx
@@ -1,23 +1,18 @@
-import { pizzas } from '/data.js'
 import { MenuItem } from './MenuItem.jsx'
 
-export function MenuList() {
+export function MenuList({ pizzas }) {
 	return (
 		<>
-			{pizzas.map(pizza => {
-				return (
-					<MenuItem
-						key={pizza.name}
-						img={pizza.imageUrl}
-						name={pizza.name}
-						ingredients={pizza.ingredients.map((ingredient, index) => {
-							return <p key={index}>{ingredient}</p>
-						})}
-						price={pizza.unitPrice}
-						sold={pizza.soldOut && true}
-					/>
-				)
-			})}
+			<MenuItem
+				key={pizzas.name}
+				img={pizzas.imageUrl}
+				name={pizzas.name}
+				ingredients={pizzas.ingredients.map((ingredient, index) => {
+					return <p key={index}>{ingredient}</p>
+				})}
+				price={pizzas.unitPrice}
+				sold={pizzas.soldOut && true}
+			/>
 		</>
 	)
 }
